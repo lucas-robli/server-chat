29e6811784a8d33732e5dbc6d417a8328b240d06
AirtonFerreira <airtonferreira@pactoebytes.com.br>
2024-03-13 11:29:01 -0300

    correcoes files v7

--- app.py
+++ app.py
@@ -9,6 +9,18 @@ import json
 app = Flask(__name__)
 CORS(app)
 
+@app.post('/chatbot')
+@cross_origin()
+def chatbot():
+    data = request.json
+    entrada_usuario = data['mensagem']
+    resposta = processar_entrada_usuario(entrada_usuario)
+    print(resposta, 'opa')
+    return jsonify(resposta)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 import os
 openai.api_key = os.getenv("OPENAIKEY")
 
